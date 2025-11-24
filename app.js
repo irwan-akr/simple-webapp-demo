3904082e7da650bc409a71453a9e819a8ededfff
Irwan Bagus Setiawan <ibagusetiawan@gmail.com>
2025-11-24 14:02:17 +0700

    v1.5

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 console.log("Login page loaded for AKR Corporindo Tbk");
+
+document.querySelector("form").addEventListener("submit", function(e){
+    e.preventDefault();
+    window.location.href = "dashboard.html";
+});
